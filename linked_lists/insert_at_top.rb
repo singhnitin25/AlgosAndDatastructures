1da318eb9e3d007e8f5b764bc4e8a34af4234c9e
Nitin S <nitin.singh@edcast.com>
2021-10-15 13:19:47 +0530

    Linked list Series - Insert at the top of the linked list.

---
+++ linked_lists/insert_at_top.rb
@@ -0,0 +1,43 @@
+class Node
+  attr_accessor :data, :next
+  def initialize(data)
+    self.data = data
+    self.next = nil
+  end
+
+  def to_s
+    "Node with value: #{@data}"
+  end
+end
+
+class LinkedList
+  def initialize
+    @head = nil
+  end
+
+  def append_at_top(data)
+    node = Node.new(data)
+    if @head
+      node.next = @head
+    end
+    @head = node
+  end
+
+  def print
+    node = @head
+    while node
+      puts node
+      node = node.next
+    end
+  end
+end
+
+
+# Case 1 when Linked List is empty i.e. Head is null
+ll = LinkedList.new
+ll.append_at_top(10)
+ll.print # 10
+
+# Case 2 when Linked list is not empty
+ll.append_at_top(20)
+ll.print # 20 -> 10
